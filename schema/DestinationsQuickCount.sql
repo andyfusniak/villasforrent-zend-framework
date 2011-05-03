5349fffb276254d53a3f9e2f48050ecafbc9003c
Andy Fusniak <andy@greycatmedia.co.uk>
2011-05-03 22:01:51 +0700

    Removed Quick count tables from schema

--- schema/DestinationsQuickCount.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS DestinationsQuickCount (
-    idDestination int(10) unsigned NOT NULL,
-    totalVisible int(10) unsigned NOT NULL DEFAULT '0',
-    total int(10) unsigned NOT NULL DEFAULT '0',
-    updated datetime NOT NULL,
-    PRIMARY KEY (idDestination),
-    KEY updated (updated),
-    CONSTRAINT destinations_quick_count_ibfk_1 FOREIGN KEY (idDestination) REFERENCES Destinations (idDestination) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
