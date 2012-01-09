062936ebb8c6e7f8e8dafbdc3b73223e6464ae7e
Andy Fusniak <andy@greycatmedia.co.uk>
2012-01-09 21:06:50 +0700

    Table renamed

--- schema/Tokens.sql
+++ schema/Tokens.sql
@@ -1,13 +1,15 @@
-CREATE TABLE IF NOT EXISTS AdvertisersReset (
-  idAdvertiserReset int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Tokens (
+  idToken int(10) unsigned NOT NULL AUTO_INCREMENT,
   idAdvertiser int(10) unsigned DEFAULT NULL,
+  type varchar(12) CHARACTER SET ascii NOT NULL,
   token varchar(60) CHARACTER SET ascii DEFAULT NULL,
   expires datetime DEFAULT NULL,
   added datetime DEFAULT NULL,
   PRIMARY KEY (idAdvertiserReset),
   UNIQUE KEY token (token),
   KEY idAdvertiser (idAdvertiser),
+  KEY type (type),
   KEY expires (expires),
   KEY added (added),
-  CONSTRAINT AdvertisersReset_ibfk_1 FOREIGN KEY (idAdvertiser) REFERENCES Advertisers (idAdvertiser) ON DELETE CASCADE ON UPDATE
+  CONSTRAINT Tokens_ibfk_1 FOREIGN KEY (idAdvertiser) REFERENCES Advertisers (idAdvertiser) ON DELETE CASCADE ON UPDATE
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
--- schema/trigger.sql
+++ schema/trigger.sql
@@ -1,4 +1,4 @@
 DROP TRIGGER IF EXISTS mycs_insert;
 DROP TRIGGER IF EXISTS mycs_update;
-CREATE TRIGGER mycs_insert BEFORE INSERT ON PropertiesContent FOR EACH ROW SET NEW.cs=SHA1(NEW.content) COLLATE utf8_persian_ci;
-CREATE TRIGGER mycs_update BEFORE UPDATE ON PropertiesContent FOR EACH ROW SET NEW.cs=SHA1(NEW.content) COLLATE utf8_persian_ci;
+CREATE TRIGGER mycs_insert BEFORE INSERT ON PropertiesContent FOR EACH ROW SET NEW.cs=SHA1(NEW.content) COLLATE utf8_general_ci;
+CREATE TRIGGER mycs_update BEFORE UPDATE ON PropertiesContent FOR EACH ROW SET NEW.cs=SHA1(NEW.content) COLLATE utf8_general_ci;
